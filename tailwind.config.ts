7955cdeb95266cfa43536c11cd35e33bd9f1095a
IbbiSecka <s355423@oslomet.no>
2025-02-15 21:50:28 +0100

    Structure created

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,11 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+      }
     },
   },
   plugins: [],
